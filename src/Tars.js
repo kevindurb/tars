ab0871f2454276a13c1bda79955bb45c71d2da2f
Kevin Durbin <kevindurb@gmail.com>
2017-10-03 15:37:08 -0600

    add event emitter and non function adapters

--- src/Tars.js
+++ src/Tars.js
@@ -1,6 +1,21 @@
-class Tars {
-  constructor(props = {}) {
-    this.adapters = props.adapters;
+const { EventEmitter } = require('events');
+
+class Tars extends EventEmitter {
+  constructor(props = {}, ...args) {
+    super(props, ...args);
+    this.adapters = props.adapters || {};
+  }
+
+  registerAdapter(name, adapter) {
+    if (typeof adapter === 'function') {
+      this.adapters[name] = adapter(this);
+    } else {
+      this.adapters[name] = adapter;
+    }
+  }
+
+  get(name) {
+    return Promise.resolve(this.adapters[name]);
   }
 }
 
--- test/Tars.test.js
+++ test/Tars.test.js
@@ -4,4 +4,33 @@ describe('Tars', () => {
   test('it should create an instance of itself', () => {
     expect(new Tars()).toBeInstanceOf(Tars);
   });
+
+  test('be able to set an adapter with a function', () => {
+    const adapter = () => 'ADAPTER';
+    const tars = new Tars();
+    tars.registerAdapter('my Adapter', adapter);
+
+    return tars.get('my Adapter').then((result) => {
+      expect(result).toEqual('ADAPTER');
+    });
+  });
+
+  test('be able to set an adapter with anything else', () => {
+    const adapter = 'ADAPTER';
+    const tars = new Tars();
+    tars.registerAdapter('my Adapter', adapter);
+
+    return tars.get('my Adapter').then((result) => {
+      expect(result).toEqual('ADAPTER');
+    });
+  });
+
+  test('should work as event emitter', () => {
+    const cb = jest.fn();
+    const tars = new Tars();
+    tars.on('event', cb);
+    tars.emit('event');
+
+    expect(cb).toBeCalled();
+  });
 });
